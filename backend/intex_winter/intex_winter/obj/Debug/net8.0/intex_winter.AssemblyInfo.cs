e226cf175765dfc44d9d88579c3f3f01a7f9c795
Caden Bird <cadenbird32@gmail.com>
2025-04-07 16:25:29 -0600

    database update

--- backend/intex_winter/intex_winter/obj/Debug/net8.0/intex_winter.AssemblyInfo.cs
+++ backend/intex_winter/intex_winter/obj/Debug/net8.0/intex_winter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("intex_winter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc70d56ebbf10203378be13187f35886c6a4d58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+887dc3c5c68806d007172902d98c2783fd34e8ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("intex_winter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("intex_winter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
