bdc36d9ed00f098bd9022cfec2a387a95f8855e3
tiaprime <tiaprime@byu.edu>
2025-04-08 09:50:41 -0600

    tia just pulled from main

--- backend/intex_winter/intex_winter/obj/Debug/net8.0/intex_winter.AssemblyInfo.cs
+++ backend/intex_winter/intex_winter/obj/Debug/net8.0/intex_winter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("intex_winter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ddedb116ca3dda9b97c16b9a50921904841a9b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783ac3f334a360cabb48c13d840b6319000b1d75")]
 [assembly: System.Reflection.AssemblyProductAttribute("intex_winter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("intex_winter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
