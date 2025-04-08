0a486910ffa7d69af281b8d94da748c8710b31b9
amymmcd <amymargaret22@gmail.com>
2025-04-07 18:59:42 -0600

    changes

--- backend/intex_winter/intex_winter/obj/Debug/net8.0/intex_winter.AssemblyInfo.cs
+++ backend/intex_winter/intex_winter/obj/Debug/net8.0/intex_winter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("intex_winter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0106c617dfb523017df1b7363dd7eae33fddc4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d87509c6630504e97cea6aadaf8e4b17dc6594fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("intex_winter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("intex_winter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
