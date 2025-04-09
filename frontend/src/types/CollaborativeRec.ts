609e9895d97e301674f2f4986d3ed03751429e18
amymmcd <amymargaret22@gmail.com>
2025-04-09 17:48:10 -0600

    collaborative filter!!!

---
+++ frontend/src/types/CollaborativeRec.ts
@@ -0,0 +1,36 @@
+export interface CollaborativeRec {
+    userId: number;
+    action: string[];
+    adventure: string[];
+    animeSeriesInternationalTVShows: string[];
+    britishTVShowsDocuseriesInternationalTVShows: string[];
+    children: string[];
+    comedies: string[];
+    comediesDramasInternationalMovies: string[];
+    comediesInternationalMovies: string[];
+    comediesRomanticMovies: string[];
+    crimeTVShowsDocuseries: string[];
+    documentaries: string[];
+    documentariesInternationalMovies: string[];
+    docuseries: string[];
+    dramas: string[];
+    dramasInternationalMovies: string[];
+    dramasRomanticMovies: string[];
+    familyMovies: string[];
+    fantasy: string[];
+    horrorMovies: string[];
+    internationalMoviesThrillers: string[];
+    internationalTVShowsRomanticTVShowsTVDramas: string[];
+    kidsTV: string[];
+    languageTVShows: string[];
+    musicals: string[];
+    natureTV: string[];
+    realityTV: string[];
+    spirituality: string[];
+    tvAction: string[];
+    tvComedies: string[];
+    tvDramas: string[];
+    talkShowsTVComedies: string[];
+    thrillers: string[];
+  }
+  
