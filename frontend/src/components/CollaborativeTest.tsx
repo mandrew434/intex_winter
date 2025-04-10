71cec6c4620b67048aa5c76368877d388184badf
tiaprime <tiaprime@byu.edu>
2025-04-09 18:04:02 -0600

    i can log in

--- frontend/src/components/CollaborativeTest.tsx
+++ frontend/src/components/CollaborativeTest.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CollaborativeRec } from '../types/CollaborativeRec';
 import { useNavigate } from 'react-router-dom';
 
